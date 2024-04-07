dff38d91c2641baafdd209bd6263a40e8a9e13e2
Catalin <mandru.catalin03@gmail.com>
2024-04-07 13:00:01 +0300

    Small fixes

--- src/components/layout/SignedInLinks.jsx
+++ src/components/layout/SignedInLinks.jsx
@@ -3,9 +3,9 @@ import {NavLink} from 'react-router-dom'
 export default function SignedInLinks() {
     return (
         <ul className="right">
-            <li><NavLink to='/'>New Project</NavLink></li>
-            <li><NavLink to='/'>Log Out</NavLink></li>
-            <li><NavLink to='/' className="btn btn-floating pink lighten-1">XX</NavLink></li>
+            <li><NavLink to="/create-project">New Project</NavLink></li>
+            <li><NavLink to="/">Sign Out</NavLink></li>
+            <li><NavLink to="/" className="btn btn-floating pink lighten-1">XX</NavLink></li>
         </ul>
     );
 }
