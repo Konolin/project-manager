adc61d28eb0c8db1a64661b95659f44a8ed7762c
Catalin <mandru.catalin03@gmail.com>
2024-04-07 16:52:19 +0300

    mor

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDifOXQ8UAbWWq_47mVyqvM0d2LWCaiywo",
+    authDomain: "project-manager-92d36.firebaseapp.com",
+    projectId: "project-manager-92d36",
+    storageBucket: "project-manager-92d36.appspot.com",
+    messagingSenderId: "495753982694",
+    appId: "1:495753982694:web:d7c092f02188b995ba8127",
+    measurementId: "G-VE7CCKVYJC"
+};
+
+initializeApp(firebaseConfig);
+const db = getFirestore();
+db.settings({timestampsInSnapshots: true});
+
+export default db;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux'
+import {configureStore} from "@reduxjs/toolkit";
 import rootReducer from "./store/reducer/rootReducer.js";
-import {Provider} from "react-redux";
 import {thunk} from "redux-thunk";
 
 const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(thunk)
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
